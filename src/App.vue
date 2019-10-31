9711f2353e114785f2d2b2bee655410dc3fedf93
Chris Dixon <chrisdixon161@googlemail.com>
2019-10-31 09:03:33 +0000

    creating the menu component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <app-header />
     <Home />
+    <Menu />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Menu from "./components/Menu";
 
 export default {
   name: "app",
   components: {
     appHeader: Header,
-    Home
+    Home,
+    Menu
   }
 };
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="menu_wrapper">
+    <!-- menu -->
+    <div class="menu">
+      <h3>~ Authentic handmade pizza ~</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <strong>~ Pepperoni ~</strong>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quis nostrum praesentium sequi perspiciatis, repudiandae dolore earum rem. Architecto magni inventore iste nostrum illum quam saepe voluptate est? Laudantium, doloremque?</small>
+            </td>
+          </tr>
+          <tr>
+            <td>9"</td>
+            <td>$6.95</td>
+            <td>
+              <button type="button" class="btn_green">+</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+
+.menu_wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu {
+  background: #f1e6da;
+  border-radius: 3px;
+  height: 100vh;
+  margin: 10px;
+  padding: 10px;
+}
+
+@media screen and (min-width: 900px) {
+  .menu_wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .menu {
+    width: 65vw;
+  }
+}
+</style>
