fdcd20940024e4bff75d2bdd9139ebb1191f4cc7
Chris Dixon <chrisdixon161@googlemail.com>
2019-11-01 09:14:42 +0000

    listing current orders

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -20,7 +20,33 @@
         </tbody>
       </table>
     </div>
-    <div class="oreder_wrapper"></div>
+    <div class="orders_wrapper">
+      <h3>Current orders (5):</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="order_number">
+            <th colspan="4">
+              <strong>Order Number: 4</strong>
+              <button type="button" class="btn_red">&times;</button>
+            </th>
+          </tr>
+          <tr>
+            <td>Margherita</td>
+            <td>9"</td>
+            <td>2</td>
+            <td>$13</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -29,3 +55,30 @@ export default {
   name: "admin"
 };
 </script>
+
+<style scoped>
+.admin_wrapper {
+  margin: 10px;
+}
+
+.current_user_wrapper,
+.order_wrapper,
+.menu_wrapper {
+  margin: 10px 0;
+  padding: 10px;
+  border: solid 1px #f79e38;
+}
+
+table {
+  text-align: left;
+  width: 70vw;
+}
+
+.order_number th {
+  background: #ddd;
+}
+
+.order_number button {
+  margin: 0 10px;
+}
+</style>
