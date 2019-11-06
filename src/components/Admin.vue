d71e762ae4a8c7133517f1d3f2a73c1469eadb62
Chris Dixon <chrisdixon161@googlemail.com>
2019-11-06 10:07:24 +0000

    signing in and out

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="admin_wrapper">
-    <div class="current_user_wrapper"></div>
+    <div class="current_user_wrapper">
+      <span>Logged in as:</span>
+      <button type="button" class="btn_red" @click.prevent="signOut">Sign out</button>
+    </div>
     <NewPizza />
     <div class="menu_wrapper">
       <h3>Menu:</h3>
@@ -55,12 +58,22 @@
 <script>
 import NewPizza from "./NewPizza";
 import Login from "./Login";
+import { firebaseAuth } from "../firebase";
 
 export default {
   name: "admin",
   components: {
     NewPizza,
     Login
+  },
+  methods: {
+    async signOut() {
+      try {
+        await firebaseAuth.signOut();
+      } catch (error) {
+        alert(`error signing out, ${error}`);
+      }
+    }
   }
 };
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,11 +3,11 @@
     <form>
       <div>
         <label for="email">Email address</label>
-        <input type="email" id="email" placeholder="Enter email" />
+        <input type="email" v-model="email" id="email" placeholder="Enter email" />
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Password" />
+        <input type="password" v-model="password" id="password" placeholder="Password" />
       </div>
       <button type="button" class="btn_green" @click.prevent="signIn">Sign in</button>
     </form>
@@ -18,8 +18,29 @@ import { firebaseAuth } from "../firebase";
 
 export default {
   name: "login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
   methods: {
-    signIn() {}
+    async signIn() {
+      try {
+        await firebaseAuth.signInWithEmailAndPassword(
+          this.email,
+          this.password
+        );
+      } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password");
+        } else {
+          alert(errorMessage);
+        }
+      }
+    }
   }
 };
 </script>
